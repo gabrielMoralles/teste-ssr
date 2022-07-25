d2c2f859684a984530887cfe2f798f0447401f36
gabrielMoralles <bimoralles@gmail.com>
2022-07-25 16:34:09 -0300

    testando ordem

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
 
+app.use(require('prerender-node').set('prerenderToken', 'XG3JPbYqZlu0HoH11ch5'));
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
 app.get('/*', (req, res) => {
 res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
 
-app.use(require('prerender-node').set('prerenderToken', 'XG3JPbYqZlu0HoH11ch5'));
 
 app.listen(process.env.PORT || 8080);
