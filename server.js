5d678e3d76926f0598cbf7a7f3fe59ddbd9d3dbc
gabrielMoralles <bimoralles@gmail.com>
2022-07-25 16:29:18 -0300

    corrigindo build

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
-const { error } = require('console');
+const nomeApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static('./dist/teste-ssr'));
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
-app.get('*', (req, res) => {
-        // res.sendFile("./dist/Marvel-Angular/index.html");
-        res.sendFile("index.html" ,{root:"./dist/teste-ssr"}); // load the single view file (angular will handle the page changes on the front-end)
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
 });
+
 app.use(require('prerender-node').set('prerenderToken', 'XG3JPbYqZlu0HoH11ch5'));
 
 app.listen(process.env.PORT || 8080);
